fae59aa34ba835903331ae5cff67c4d32a8ea98b
vijay4145 <svijay4145@gmail.com>
2022-10-07 17:42:41 +0530

    added fcm notification

--- app/src/main/java/com/example/askyoursenior/Homepage.java
+++ app/src/main/java/com/example/askyoursenior/Homepage.java
@@ -1,14 +1,23 @@
 package com.example.askyoursenior;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.example.askyoursenior.databinding.ActivityHomepageBinding;
 import com.example.askyoursenior.homepage_fragments.CollabProjectFragment;
 import com.example.askyoursenior.homepage_fragments.HomeFragment;
 import com.example.askyoursenior.homepage_fragments.profilepackage.ProfileFragment;
+import com.example.askyoursenior.model.SharedPreferenceDb;
+import com.example.askyoursenior.notifications.NotificationOfNewUserRegisteredOnSameOrganization;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class Homepage extends AppCompatActivity {
     ActivityHomepageBinding homepageBinding;
@@ -16,6 +25,33 @@ public class Homepage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         homepageBinding = DataBindingUtil.setContentView(this, R.layout.activity_homepage);
+        SharedPreferences getshared = getSharedPreferences(SharedPreferenceDb.DB_NAME, MODE_PRIVATE);
+        String val = getshared.getString(SharedPreferenceDb.ORGANIZATION_NAME, "NOT_FOUND");
+        NotificationOfNewUserRegisteredOnSameOrganization.subscribe(val);
+
+
+
+//        -----------------------------
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(Homepage.this, "token not generated", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+
+                        // Get new FCM registration token
+                        String token = task.getResult();
+
+                        // Log and toast
+                        homepageBinding.token.setText(token);
+                        homepageBinding.token1.setText(token);
+                        Toast.makeText(Homepage.this, "succesful", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+//        -------------------
 
 
         HomeFragment fragmenthome = new HomeFragment();
@@ -37,4 +73,5 @@ public class Homepage extends AppCompatActivity {
 
 
     }
+
 }
